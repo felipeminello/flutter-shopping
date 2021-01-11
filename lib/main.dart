ceef276157f4544e17ab1a778b967483d4be9699
Felipe Minello <felipeminello@gmail.com>
2021-01-11 12:40:26 -0300

    tabs page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping/blocs/home.bloc.dart';
-import 'package:shopping/ui/shared/widgets/category/category-list.widget.dart';
-import 'package:shopping/ui/shared/widgets/product/product-list.widget.dart';
+import 'package:shopping/ui/android/pages/tabs.page.dart';
 
 void main() {
   runApp(MyApp());
@@ -33,50 +32,10 @@ class Main extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final HomeBloc bloc = Provider.of<HomeBloc>(context);
-
-    return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          SizedBox(
-            height: 60,
-          ),
-          Text(
-            'Categorias',
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          CategoryList(
-            categories: bloc.categories,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Mais vendidos',
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          ProductList(
-            products: bloc.products,
-          ),
-        ],
+      home: DefaultTabController(
+        length: 3,
+        child: TabsPage(),
       ),
-    ));
+    );
   }
 }
